09b6ab5040826a94fb81b45c1d4fd5ff4346c370
Renato Campos <renato@polymathean.org>
2022-09-26 13:21:39 -0700

    Delete directory.

--- cs01/lab1/MyProgram.java
+++
@@ -1,8 +0,0 @@
-// I wrote this program late, but I will change it
-// so it looks like I wrote it early!  Hee, hee.
-
-public class MyProgram {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
-}
